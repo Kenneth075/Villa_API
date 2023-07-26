f710fc71faed17b74d479a9c3aa1a4c61adec19d
Kenneth Edoho <131462290+Kenneth075@users.noreply.github.com>
2023-07-26 12:04:49 +0100

    Commit all

--- MagicVilla_KennyAPI/Program.cs
+++ MagicVilla_KennyAPI/Program.cs
@@ -2,7 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(option => { option.ReturnHttpNotAcceptable = true; }).AddNewtonsoftJson();
+builder.Services.AddControllers(option => { 
+    option.ReturnHttpNotAcceptable = true; 
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();  //Adding xlm format to our api.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
